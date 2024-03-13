5f49ceef3601d21b2de026397d57f379a54872b0
hasanarpat <black_the_dragon@hotmail.com>
2024-03-13 18:03:09 +0300

    Fix carousel component to be able to  act on different situations and props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,70 @@
 import Carousel from '@/components/shared/Carousel';
 const array1 = [1, 2, 3];
-const array2 = [1, 2, 3, 4, 5, 6];
+const array2 = [
+  {
+    id: 1833,
+    title: 'Tatlı Keçi',
+    description: 'Keçi işte dünyanın en güzel şeylerinden',
+    image:
+      'https://images.pexels.com/photos/13497825/pexels-photo-13497825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: 'Satılık değil!!!',
+    category: 'Cute',
+  },
+  {
+    id: 1833,
+    title: 'Süper Ördek',
+    description: 'Sihirli güçleriyle Gotham şehrini korur',
+    image:
+      'https://images.pexels.com/photos/2397623/pexels-photo-2397623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: 'Satılık değil!!!',
+    category: 'Cute',
+  },
+  {
+    id: 1833,
+    title: 'Ceket falan',
+    description: 'Modern tarzda slim fit erkek pantolon',
+    image:
+      'https://images.pexels.com/photos/3522691/pexels-photo-3522691.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: '$25',
+    category: 'jacket',
+  },
+  {
+    id: 1833,
+    title: 'Erkek Slim Fit Pantolon',
+    description: 'Modern tarzda slim fit erkek pantolon',
+    image:
+      'https://images.pexels.com/photos/20220617/pexels-photo-20220617/free-photo-of-siyah-ve-beyaz-moda-adam-insanlar.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: '$25',
+    category: 'jacket',
+  },
+  {
+    id: 1833,
+    title: 'Erkek Slim Fit Pantolon',
+    description: 'Modern tarzda slim fit erkek pantolon',
+    image:
+      'https://images.pexels.com/photos/18254632/pexels-photo-18254632/free-photo-of-siyah-ve-beyaz-moda-kadin-model.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: '$25',
+    category: 'jacket',
+  },
+  {
+    id: 1833,
+    title: 'Erkek Slim Fit Pantolon',
+    description: 'Modern tarzda slim fit erkek pantolon',
+    image:
+      'https://images.pexels.com/photos/19806386/pexels-photo-19806386/free-photo-of-siyah-ve-beyaz-adam-insanlar-kadin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: '$25',
+    category: 'jacket',
+  },
+  {
+    id: 1833,
+    title: 'Erkek Slim Fit Pantolon',
+    description: 'Modern tarzda slim fit erkek pantolon',
+    image:
+      'https://images.pexels.com/photos/19806386/pexels-photo-19806386/free-photo-of-siyah-ve-beyaz-adam-insanlar-kadin.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cost: '$25',
+    category: 'jacket',
+  },
+];
 export default function Home() {
   return (
     <main className=''>
@@ -11,6 +75,7 @@ export default function Home() {
           gelsin!
         </h1>
         <Carousel
+          isProduct={false}
           array={array1}
           classNameContainer='p-2 lg:p-2'
           classNameCarousel='max-w-none md:max-w-none lg:max-w-[70%] w-[calc(100%-6rem)]'
@@ -20,14 +85,47 @@ export default function Home() {
           classNameCardContent='aspect-video lg:aspect-[16/6]'
         />
       </section>
-      <section>
+      <section className='p-4 lg:px-24'>
         {/* SLIDERS */}
         <Carousel
-          classNameContainer='p-3 lg:p-12 min-h-[220px]'
+          isProduct={true}
+          classNameContainer='p-3 lg:p-12 lg:py-2 min-h-[220px]'
           classNameCardContent=''
           classNameCard='hover:bg-emerald-500 hover:-translate-y-2 ease-in-out duration-100'
           array={array2}
-          title='Jackets'
+          title='Trend'
+        />
+        <Carousel
+          isProduct={true}
+          classNameContainer='p-3 lg:p-12 lg:py-2 min-h-[220px]'
+          classNameCardContent=''
+          classNameCard='hover:bg-emerald-500 hover:-translate-y-2 ease-in-out duration-100'
+          array={array2}
+          title='New'
+        />
+        <Carousel
+          isProduct={true}
+          classNameContainer='p-3 lg:p-12 lg:py-2 min-h-[220px]'
+          classNameCardContent=''
+          classNameCard='hover:bg-emerald-500 hover:-translate-y-2 ease-in-out duration-100'
+          array={array2}
+          title='Hard Rock'
+        />
+        <Carousel
+          isProduct={true}
+          classNameContainer='p-3 lg:p-12 lg:py-2 min-h-[220px]'
+          classNameCardContent=''
+          classNameCard='hover:bg-emerald-500 hover:-translate-y-2 ease-in-out duration-100'
+          array={array2}
+          title='Casettes'
+        />
+        <Carousel
+          isProduct={true}
+          classNameContainer='p-3 lg:p-12 lg:py-2 min-h-[220px]'
+          classNameCardContent=''
+          classNameCard='hover:bg-emerald-500 hover:-translate-y-2 ease-in-out duration-100'
+          array={array2}
+          title='Accessories'
         />
       </section>
     </main>
--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import {
   Carousel,
   CarouselContent,
@@ -6,7 +13,15 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+
 import { cn } from '@/lib/utils';
+import { TProduct } from '@/types';
+
+import { CiShoppingCart } from 'react-icons/ci';
+import { CiHeart } from 'react-icons/ci';
+import { IoTicketOutline } from 'react-icons/io5';
+
+import Image from 'next/image';
 import Link from 'next/link';
 
 const CarouselComponent = (props: {
@@ -20,8 +35,9 @@ const CarouselComponent = (props: {
   classNameCardDescription?: String;
   classNameCardTitle?: String;
   classNameCardFooter?: String;
-  array: number[];
+  array: number[] | TProduct[];
   title?: String;
+  isProduct: boolean;
 }) => {
   return (
     <div
@@ -31,7 +47,7 @@ const CarouselComponent = (props: {
       )}
     >
       {props.title && (
-        <div className='w-full text-emerald-500 mb-4 text-xl font-bold text-center md:text-start'>
+        <div className='w-full text-emerald-500 mb-4 text-xl font-bold text-center lg:text-start'>
           <h3>
             <Link href={`/categories/${props.title.toLowerCase()}`}>
               {props.title}
@@ -46,28 +62,99 @@ const CarouselComponent = (props: {
         )}
       >
         <CarouselContent className={cn('', props.classNameContent)}>
-          {Array.from({ length: props.array?.length }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className={cn(
-                'basis-full md:basis-1/3 lg:basis-1/4 xl:basis-1/6',
-                props.classNameItem
-              )}
-            >
-              <div className='p-1'>
-                <Card className={cn('', props.classNameCard)}>
-                  <CardContent
-                    className={cn(
-                      'flex aspect-square items-center justify-center p-6',
-                      props.classNameCardContent
-                    )}
-                  >
-                    <span className='text-4xl font-semibold'>{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
+          {props.isProduct
+            ? (props.array as TProduct[]).map((item, index) => (
+                <CarouselItem
+                  key={index}
+                  className={cn(
+                    'basis-full md:basis-1/2 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6',
+                    props.classNameItem
+                  )}
+                >
+                  <div className='p-1'>
+                    <Card className={cn('', props.classNameCard)}>
+                      <CardContent
+                        className={cn(
+                          'flex aspect-square items-center justify-center p-0',
+                          props.classNameCardContent
+                        )}
+                      >
+                        <Link
+                          className='w-full h-full relative'
+                          href={`/products/${item.id}`}
+                        >
+                          <Image
+                            alt='product'
+                            src={item.image}
+                            fill
+                            className='object-cover'
+                          />
+                        </Link>
+                      </CardContent>
+                      <CardHeader className={cn('', props.classNameCardHeader)}>
+                        <CardTitle className={cn('', props.classNameCardTitle)}>
+                          {item.title}
+                        </CardTitle>
+                        <CardDescription
+                          className={cn('', props.classNameCardDescription)}
+                        >
+                          {item.description}
+                        </CardDescription>
+                      </CardHeader>
+                      <CardFooter className='p-0'>
+                        <div className='w-full flex flex-col items-center justify-between gap-4'>
+                          <div className='w-full flex items-center justify-between px-1'>
+                            <span className='w-full text-center'>
+                              {item.cost}
+                            </span>
+                            <span className='w-full text-center flex items-center'>
+                              <Link
+                                href={`/categories/${item.category}`}
+                                className='hover:text-emerald-200'
+                              >
+                                {item.category}
+                              </Link>
+                              <IoTicketOutline className='text-2xl' />
+                            </span>
+                          </div>
+                          <div className='w-full mb-2 gap-4 flex items-center justify-center text-3xl'>
+                            <span className='text-yellow-500 hover:stroke-yellow-500'>
+                              <CiShoppingCart />
+                            </span>
+                            <span className='text-red-500'>
+                              <CiHeart />
+                            </span>
+                          </div>
+                        </div>
+                      </CardFooter>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))
+            : Array.from({ length: props.array?.length }).map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className={cn(
+                    'basis-full md:basis-1/3 lg:basis-1/4 xl:basis-1/6',
+                    props.classNameItem
+                  )}
+                >
+                  <div className='p-1'>
+                    <Card className={cn('', props.classNameCard)}>
+                      <CardContent
+                        className={cn(
+                          'flex aspect-square items-center justify-center p-6',
+                          props.classNameCardContent
+                        )}
+                      >
+                        <span className='text-4xl font-semibold'>
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
