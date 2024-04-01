92b3575ed8d416ecb3f4549a78459d4f2780aec6
hasanarpat <black_the_dragon@hotmail.com>
2024-04-01 12:38:06 +0300

    style sign pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className='w-full h-screen grid place-content-center bg-gradient-to-br from-emerald-500 to-cyan-600'>
+      <SignIn />
+    </div>
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className='w-full h-screen grid place-content-center'>
+    <div className='w-full h-screen grid place-content-center bg-gradient-to-br from-cyan-600 to-emerald-500'>
       <SignUp />
     </div>
   );
