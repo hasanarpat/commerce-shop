606cbdee7fbe7b0daac7d390f018e60bab19e65f
hasanarpat <black_the_dragon@hotmail.com>
2024-12-22 00:32:25 +0300

    adjust type on search page

--- src/app/(site)/search/page.tsx
+++ src/app/(site)/search/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import React from 'react';
-import { cn } from '@/lib/utils';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/lib/utils";
 
 import {
   Pagination,
@@ -11,32 +11,32 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from '@/components/ui/pagination';
-import GridCards from '@/components/shared/GridCards';
-import { array3 } from '../../data';
+} from "@/components/ui/pagination";
+import GridCards from "@/components/shared/GridCards";
+import { array3 } from "../../data";
 
 const SearchResultsPage = (searchParams: {
   [key: string]: string | string[] | undefined;
 }) => {
-  const item = searchParams.searchParams && searchParams.searchParams['item'];
+  const item = searchParams.searchParams && searchParams.searchParams["item"];
   const latest =
-    searchParams.searchParams && searchParams.searchParams['latest'];
+    searchParams.searchParams && searchParams.searchParams["latest"];
   const cheapest =
-    searchParams.searchParams && searchParams.searchParams['cheapest'];
+    searchParams.searchParams && searchParams.searchParams["cheapest"];
   const inOffer =
-    searchParams.searchParams && searchParams.searchParams['inOffer'];
+    searchParams.searchParams && searchParams.searchParams["inOffer"];
 
   return (
-    <div className='p-2 md:p-4 lg:p-12 xl:px-16'>
-      <div className='flex flex-col items-start gap-4 lg:gap-8 justify-center'>
-        <div className='md:w-1/3 lg:w-1/2 w-full mx-auto flex items-center justify-center gap-4 flex-wrap'>
+    <div className="p-2 md:p-4 lg:p-12 xl:px-16">
+      <div className="flex flex-col items-start gap-4 lg:gap-8 justify-center">
+        <div className="md:w-1/3 lg:w-1/2 w-full mx-auto flex items-center justify-center gap-4 flex-wrap">
           <Link
             href={{
               pathname: `/search/`,
               query: {
                 item,
                 latest:
-                  latest !== undefined && latest === 'true' ? 'false' : true,
+                  latest !== undefined && latest === "true" ? "false" : true,
                 cheapest,
                 inOffer,
               },
@@ -45,8 +45,8 @@ const SearchResultsPage = (searchParams: {
             <Button
               className={cn(
                 latest !== undefined &&
-                  latest === 'true' &&
-                  'bg-emerald-500 text-white'
+                  latest === "true" &&
+                  "bg-emerald-500 text-white"
               )}
             >
               Yeniden Eskiye Sırala
@@ -59,8 +59,8 @@ const SearchResultsPage = (searchParams: {
                 item,
                 latest,
                 cheapest:
-                  cheapest !== undefined && cheapest === 'true'
-                    ? 'false'
+                  cheapest !== undefined && cheapest === "true"
+                    ? "false"
                     : true,
                 inOffer,
               },
@@ -69,8 +69,8 @@ const SearchResultsPage = (searchParams: {
             <Button
               className={cn(
                 cheapest !== undefined &&
-                  cheapest === 'true' &&
-                  'bg-emerald-500 text-white'
+                  cheapest === "true" &&
+                  "bg-emerald-500 text-white"
               )}
             >
               Ucuzdan Pahalıya Sırala
@@ -84,46 +84,46 @@ const SearchResultsPage = (searchParams: {
                 latest,
                 cheapest,
                 inOffer:
-                  inOffer !== undefined && inOffer === 'true' ? 'false' : true,
+                  inOffer !== undefined && inOffer === "true" ? "false" : true,
               },
             }}
           >
             <Button
               className={cn(
                 inOffer !== undefined &&
-                  inOffer === 'true' &&
-                  'bg-emerald-500 text-white'
+                  inOffer === "true" &&
+                  "bg-emerald-500 text-white"
               )}
             >
               Kampanyalı Ürünler
             </Button>
           </Link>
         </div>
-        <div className='lg:w-[90%] xl:w-[80%] w-full mx-auto'>
+        <div className="lg:w-[90%] xl:w-[80%] w-full mx-auto">
           <GridCards items={[...array3]} />
         </div>
-        <div className='bg-secondary w-full mb-2'>
+        <div className="bg-secondary w-full mb-2">
           <Pagination>
             <PaginationContent>
               <PaginationItem>
-                <PaginationPrevious href='#' />
+                <PaginationPrevious href="#" />
               </PaginationItem>
               <PaginationItem>
-                <PaginationLink href='#'>1</PaginationLink>
+                <PaginationLink href="#">1</PaginationLink>
               </PaginationItem>
               <PaginationItem>
-                <PaginationLink href='#' isActive>
+                <PaginationLink href="#" isActive>
                   2
                 </PaginationLink>
               </PaginationItem>
               <PaginationItem>
-                <PaginationLink href='#'>3</PaginationLink>
+                <PaginationLink href="#">3</PaginationLink>
               </PaginationItem>
               <PaginationItem>
                 <PaginationEllipsis />
               </PaginationItem>
               <PaginationItem>
-                <PaginationNext href='#' />
+                <PaginationNext href="#" />
               </PaginationItem>
             </PaginationContent>
           </Pagination>
