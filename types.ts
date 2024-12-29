307e4816da7663014f3f16488bdb0b5099838921
hasanarpat <black_the_dragon@hotmail.com>
2024-12-29 16:40:22 +0300

    ajust types

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type TProduct = {
+  id: string; // Change from number to string
+  title: string;
+  description: string;
+  images: string[];
+  cost: string;
+  category: string;
+  size: string[];
+  isInOffer: boolean;
+  offer: string;
+  gender: string;
+};
